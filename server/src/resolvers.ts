5bacbcd638e57b7201a40ad7841f06d0399f5bbf
Cam Lindsay <camlindsay5@gmail.com>
2022-08-19 14:45:18 +0100

    work

---
+++ server/src/resolvers.ts
@@ -0,0 +1,24 @@
+import queryTwitterResolvers from "./resolvers/Query"
+import { Resolvers } from "./resolvers-types.generated"
+import Db, { DbTweet, DbUser } from "./db"
+import tweetTwitterResolver from './resolvers/Tweet';
+import userTwitterResolver from './resolvers/User';
+import mutationTwitterResolver from "./resolvers/Mutation";
+
+
+export interface TwitterResolverContext {
+  db: Db
+  dbTweetCache: Record<string, DbTweet>
+  dbUserCache: Record<string, DbUser>
+  dbTweetToFavoriteCountMap: Record<string, number>
+}
+
+const resolvers: Resolvers<TwitterResolverContext> = {
+  Query: queryTwitterResolvers,
+  Tweet: tweetTwitterResolver,
+  User: userTwitterResolver,
+ Mutation: mutationTwitterResolver
+}
+
+
+export default resolvers
